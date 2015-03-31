4368220602edc86399b70ab8b56b120b69adc730
David Castro <arimus@gmail.com>
2015-03-31 13:26:06 -0700

    updated code igniter version

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -57,7 +57,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = '../ci2.0.2';
+	$system_path = '../ci2.1.4';
 
 /*
  *---------------------------------------------------------------
