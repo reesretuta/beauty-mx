3af3b5979ebb8510083fe0cff9e350490abecdf7
David Castro <arimus@gmail.com>
2015-04-19 22:46:09 -0700

    crazyegg tag

--- applications/frontend/views/homeView.php
+++ applications/frontend/views/homeView.php
@@ -1502,5 +1502,12 @@
 <script type="text/javascript">(function () {
         return window.SIG_EXT = {};
       })()</script>
-      
-</body></html>
+
+<script type="text/javascript">
+    setTimeout(function(){var a=document.createElement("script");
+        var b=document.getElementsByTagName("script")[0];
+        a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0032/4722.js?"+Math.floor(new Date().getTime()/3600000);
+        a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
+
+  </body></html>
